8f487239a58934aae03cc60538814585460554ec
getjvihaan-hue <getjvihaan@gmail.com>
2026-07-03 16:51:48 +0800

    Create Aircraft-data.js

---
+++ Aircraft-data.js
@@ -0,0 +1,23 @@
+const aircraft = [
+  {
+    name: "Boeing 737-800",
+    engines: 2,
+    type: "Narrow-body jet",
+    manufacturer: "Boeing",
+    first_flight: 1997
+  },
+  {
+    name: "Airbus A320",
+    engines: 2,
+    type: "Narrow-body jet",
+    manufacturer: "Airbus",
+    first_flight: 1987
+  },
+  {
+    name: "Boeing 787 Dreamliner",
+    engines: 2,
+    type: "Wide-body jet",
+    manufacturer: "Boeing",
+    first_flight: 2009
+  }
+];
